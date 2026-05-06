64f91b21e369a3ed56bda5264c8786800d062d7a
weiwoduxing <2010651761@qq.com>
2026-05-06 15:57:55 +0800

    Update Actions.svelte

--- src/components/Controls/ActionBar/Actions.svelte
+++ src/components/Controls/ActionBar/Actions.svelte
@@ -19,17 +19,25 @@
 			userGrid.applyHint($cursor);
 		}
 	}
+
+	function handleUndo() {
+		userGrid.undo();
+	}
+
+	function handleRedo() {
+		userGrid.redo();
+	}
 </script>
 
 <div class="action-buttons space-x-3">
 
-	<button class="btn btn-round" disabled={$gamePaused} title="Undo">
+	<button class="btn btn-round" disabled={$gamePaused} on:click={handleUndo} title="Undo">
 		<svg class="icon-outline" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
 			<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 10h10a8 8 0 018 8v2M3 10l6 6m-6-6l6-6" />
 		</svg>
 	</button>
 
-	<button class="btn btn-round" disabled={$gamePaused} title="Redo">
+	<button class="btn btn-round" disabled={$gamePaused} on:click={handleRedo} title="Redo">
 		<svg class="icon-outline" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
 			<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 10h-10a8 8 90 00-8 8v2M21 10l-6 6m6-6l-6-6" />
 		</svg>
